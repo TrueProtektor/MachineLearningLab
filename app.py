4b8babeb13d8fdfab9062de6bb6a66dd13c5618c
stfu-m8 <shadowyogurt@gmail.com>
2022-11-27 16:27:17 +0500

    refactor: rewrote app using transformers pipelines

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
-from transformers import DetrFeatureExtractor, DetrForObjectDetection
+from transformers import DetrFeatureExtractor, DetrForObjectDetection, pipeline, ImageClassificationPipeline
+from operator import itemgetter
 import torch
 from PIL import Image
 import PIL
@@ -6,8 +7,7 @@ import requests
 import numpy as np
 import streamlit as st
 
-feature_extractor = DetrFeatureExtractor.from_pretrained("facebook/detr-resnet-50")
-model = DetrForObjectDetection.from_pretrained("facebook/detr-resnet-50")
+classify: ImageClassificationPipeline = pipeline('object-detection', model='facebook/detr-resnet-50', feature_extractor='facebook/detr-resnet-50')
 
 uploaded_image = st.file_uploader(label='Загрузить изображение', type=['png', 'jpg'])
 if (uploaded_image is not None):
@@ -15,22 +15,18 @@ if (uploaded_image is not None):
 	st.caption('Исходное изображение')
 	st.image(image)
 
-	inputs = feature_extractor(images=image, return_tensors="pt")
-	outputs = model(**inputs)
-
 	target_sizes = torch.tensor([image.size[::-1]])
-	results = feature_extractor.post_process(outputs, target_sizes=target_sizes)[0]
+	results = classify(image)
 
 	object_detected = False
-	for score, label, box in zip(results["scores"], results["labels"], results["boxes"]):
-		box = [round(i, 2) for i in box.tolist()]
+	for result in results:
+		score, label, box = itemgetter('score', 'label', 'box')(result)
 		img_draw = PIL.ImageDraw.Draw(image)
 		if score > 0.9:
 			object_detected = True
-			img_draw.rectangle([box[0], box[1], box[2], box[3]], outline='red')
+			img_draw.rectangle([box['xmin'], box['ymin'], box['xmax'], box['ymax']], outline='red')
 			
-			name = model.config.id2label[label.item()]
-			img_draw.text([box[0], box[1]], name)
+			img_draw.text([box['xmin'], box['ymin']], label)
 			pass
 
 	if (object_detected):
